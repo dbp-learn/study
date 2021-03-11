e7f52ee611df20447d3785ff2ac69b019ca900d8
Vladimir Dulov <v.dulov@gmail.com>
2021-03-11 12:48:51 +0400

    replace() and search() methods (binary search) implemented

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -2,11 +2,27 @@ module Exercise
   module Arrays
     class << self
       def replace(array)
-        array
+        max_value = array.sort.last
+        # max_value = array.reduce(array.first) { |max_val, val| max_val > val ? max_val : val }
+        array.map { |val| val.positive? ? max_value : val }
       end
 
-      def search(_array, _query)
-        0
+      def my_bisearch(array, query, left, right)
+        middle = (left + (right - left) / 2).to_i
+
+        return middle if array[middle] == query
+
+        return -1 if left == right
+
+        return my_bisearch(array, query, left, middle) if array[middle] > query
+
+        my_bisearch(array, query, middle + 1, right)
+      end
+
+      def search(array, query)
+        return -1 if array.empty?
+
+        my_bisearch(array, query, 0, array.length - 1)
       end
     end
   end
