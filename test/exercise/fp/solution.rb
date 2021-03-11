77ffdaf43ccd116bff6cfa0c002dede86c707c51
Vladimir Dulov <v.dulov@gmail.com>
2021-03-11 14:42:46 +0400

    logic refactored

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,13 +5,14 @@ module Exercise
       # film['name'], film['rating_kinopoisk'], film['rating_imdb'],
       # film['genres'], film['year'], film['access_level'], film['country']
       def rating(films)
-        selected_films = films.select { |film| film['rating_kinopoisk'].to_i.positive? && film['country'].to_s.split(',').size > 1 }
-        selected_films.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f } / selected_films.size
+        selected_films = films.select { |film| film['rating_kinopoisk'].to_f.positive? && film['country']&.include?(',') }
+        ratings_sum = selected_films.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f }
+        ratings_sum / selected_films.size
       end
 
       def chars_count(films, threshold)
         selected_films = films.select { |film| film['rating_kinopoisk'].to_f >= threshold }
-        selected_films.reduce(0) { |sum, film| sum + film['name'].count('и') }
+        selected_films.sum { |film| film['name'].count('и') }
       end
     end
   end
